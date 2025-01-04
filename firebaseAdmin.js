9eedb4cac7e4c353373f419925d12dd6ebcc93a1
TusharRaval <ravaltushar11@gmail.com>
2025-01-04 23:05:39 +0530

    Update fir

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,1 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("./todos-46a7e-firebase-adminsdk-yod24-2d07d5d655.json"); // Use your downloaded key
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
-
-const db = admin.firestore();
-const tasksCollection = db.collection("tasks");
-
-module.exports = { db, tasksCollection };
